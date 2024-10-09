d76a64a5cb5b734dd285ff6cf7f99febf4aeb892
Ardis160 <andrewpatro@seznam.cz>
2024-10-09 23:47:20 +0200

    added the gauss function

--- gauss.py
+++ gauss.py
@@ -6,25 +6,28 @@ a = np.array([[2, 2, 0],
 
 b = np.array([5, 3, 7], float)
 
-x = np.linalg.solve(a, b)
-print("Koeficienty jsou:")
-print(x)
+n = len(b)
 
+for i in range(n):
+    for j in range(i + 1, n):
+        multiplier = a[j, i] / a[i, i]
+        for k in range(i, n):
+            a[j, k] -= multiplier * a[i, k]
+        b[j] -= multiplier * b[i]
 
+print(a)
 
-"""n = len(b)
-for k in range(n):
-    for i in range(k + 1, n):
-        fctr = a[i, k] / a[k, k]
-        for j in range(k, n):
-            a[i, j] -= fctr * a[k, j]
-        b[i] -= fctr * b[k]
-
-# Zpětná substituce
 x = np.zeros(n)
+
 for i in range(n - 1, -1, -1):
     x[i] = (b[i] - np.dot(a[i, i + 1:], x[i + 1:])) / a[i, i]
 
 print("Koeficienty jsou")
-print(x)"""
+print(x)
+
+"""
+y = np.linalg.solve(a, b)
+
+print("Koeficienty jsou:")
+print(y)"""
 
